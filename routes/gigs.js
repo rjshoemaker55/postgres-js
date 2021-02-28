98a9a6e93d71c467f6d4abf75b576882f28bb673
RJ Shoemaker <rjshoemaker55@gmail.com>
2021-02-28 17:21:13 -0500

    sequelize-cli wokring, updated database and all files

--- routes/gigs.js
+++ routes/gigs.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../config/database')
-const Gig = require('../models/gig')
+const { Gig } = require('../models')
 
 // Get gig list
 router.get('/', (req, res) => {
